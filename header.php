4cce85c7728b6bfe3b00b6bdb89f0029896c5466
grange85 <andy@grange85.co.uk>
2016-11-21 12:27:14 +0000

    remove old ga code and add tag manager code to head and body

--- header.php
+++ header.php
@@ -53,6 +53,15 @@
           content="<?php echo get_template_directory_uri(); ?>/images/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NPD7LQG');</script>
+<!-- End Google Tag Manager -->
+
+
     <!-- Respond.js proxy on external server -->
     <link href="http://externalcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
 
@@ -92,20 +101,14 @@
     $options = get_option('logo_options_group');
 
     ?>
-    <!-- Google Analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-2827241-12', 'exploreyourarchive.org');
-        ga('send', 'pageview');
-
-    </script>
 
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-Top">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPD7LQG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div id="fb-root"></div>
 <script>(function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
